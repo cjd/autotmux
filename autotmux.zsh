dd87c79850038aa6dca740d72a612bf50585fa70
Chris Debenham <chris@adebenham.com>
2023-11-13 07:36:01 +1100

    Run tmux even for local sessions

--- autotmux.zsh
+++ autotmux.zsh
@@ -1,5 +1,5 @@
 takeover() {
-  create a temporary session that displays the "how to go back" message
+  # create a temporary session that displays the "how to go back" message
   tmp='takeover temp session'
   if ! $TMX has-session -t "$tmp"; then
     $TMX new-session -d -s "$tmp"
@@ -13,10 +13,23 @@ takeover() {
   done
 }
 
+# Remote connection
 SESSIONNAME=`cat /etc/hostname | cut -f1 -d"."`
 
 if [ -n "$SSH_CONNECTION" ] && [ -z "$TMUX" ] && [ $TERM != 'dumb' ]; then
   TMX='tmux'
   command -v tmx2 > /dev/null && TMX='tmx2'
   if $($TMX ls | grep -q $SESSIONNAME); then takeover; exec $TMX attach -t $SESSIONNAME; else exec $TMX new -s $SESSIONNAME;fi
+fi
+
+# Local connection
+if [ -z "$TMUX" ]; then
+  if `tmux has-session`; then
+    SESSION=`tmux list-sessions | grep -v attached | head -1 | cut -f1 -d:`
+    if [ -n "$SESSION" ]; then
+      exec tmux attach -t $SESSION
+    else exec tmux
+    fi
+  else exec tmux
+  fi
 fi
